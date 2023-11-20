1fd0ca72cf13848845502c32549fc14214d9b3ca
adelintang <adelintang157@gmail.com>
2023-11-20 07:00:23 +0700

    add revision in e2e test, display data in thread comment

--- src/components/ThreadComment.jsx
+++ src/components/ThreadComment.jsx
@@ -9,13 +9,24 @@ const ThreadComment = ({ comment }) => {
     <div className="shadow bg-white grid gap-y-3 p-3 rounded">
       <UserCreated avatar={comment.owner.avatar} name={comment.owner.name} />
       <div>{parser(comment.content)}</div>
-      <p className="text-[#888]">{postedAt(comment.created)}</p>
+      <p className="text-[#888]">{postedAt(comment.createdAt)}</p>
     </div>
   );
 };
 
 ThreadComment.propTypes = {
-  comment: PropTypes.object.isRequired,
+  comment: PropTypes.shape({
+    content: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    downVotesBy: PropTypes.array.isRequired,
+    id: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+    upVotesBy: PropTypes.array.isRequired,
+  }).isRequired,
 };
 
 export default ThreadComment;
